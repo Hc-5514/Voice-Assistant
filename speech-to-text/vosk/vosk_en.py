25e764761560eb702e7995d93c91cd6f3e85cc4f
Hc-5514 <tbd05050@naver.com>
2025-02-27 11:45:25 +0900

    feat: stt 개발 (vosk)
    사용 모델: vosk-model-small-en
    성능: 짧은 영단어 인식률 높음, 문장 인식률 낮음

---
+++ speech-to-text/vosk/vosk_en.py
@@ -0,0 +1,14 @@
+import pyaudio
+from vosk import Model, KaldiRecognizer
+
+model = Model("./models/vosk-model-en")     # 한국어 모델 사용
+recognizer = KaldiRecognizer(model, 16000)  # 16kHz 설정
+mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+stream.start_stream()
+
+print("말하세요...")
+while True:
+    data = stream.read(4096)
+    if recognizer.AcceptWaveform(data):
+        print(recognizer.Result())
